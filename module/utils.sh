206399b2222bcf3c3f3cfd472491d035c0ce709c
m4mental <github-action@actions-template-sync.noreply.github.com>
2026-09-06 18:24:38 +0000

    chore(template): merge template changes :up:
    Signed-off-by: m4mental <github-action@actions-template-sync.noreply.github.com>

--- module/utils.sh
+++ module/utils.sh
@@ -7,9 +7,9 @@ ch_desc() {
 	sed -i "s|^description=.*|description=${1}|" "$MODDIR/module.prop"
 }
 
-if su -M -c true >/dev/null 2>/dev/null; then
-	alias mm='su -M -c'
-else alias mm='nsenter -t1 -m'; fi
+ch_desc_err() {
+	ch_desc "⚠️ Needs reflash: '${1}'"
+}
 
 pmex() {
 	OP=$(pm "$@" 2>&1 </dev/null)
@@ -33,30 +33,30 @@ get_basepath() {
 }
 
 umount_all() {
-	mm grep -F "$PKG_NAME" /proc/mounts | while read -r line; do
+	su -M -c grep -F "$PKG_NAME" /proc/mounts | while read -r line; do
 		mp=${line#* } mp=${mp%% *} mp=${mp%%\\*}
-		mm umount -l "${mp}"
+		su -M -c umount -l "${mp}"
 	done
 	am force-stop "$PKG_NAME" || :
 }
 
 get_mounts() {
-	mm grep -F "$PKG_NAME" /proc/mounts || :
+	su -M -c grep -F "$PKG_NAME" /proc/mounts || :
 }
 
 mount_rv() {
 	if [ ! -d "${1}/lib" ]; then
-		err "Your installation got broken. Dont report this, consider using rvmm-zygisk-mount."
+		ch_desc_err "Your installation got broken. Dont report this, consider using rvmm-zygisk-mount."
 		return 1
 	fi
 	VERSION=$(get_app_version)
 	if [ "$VERSION" != "$PKG_VER" ] && [ "$VERSION" ]; then
-		err "version mismatch (installed:$VERSION, module:$PKG_VER)"
+		ch_desc_err "Version mismatch (installed:$VERSION, module:$PKG_VER)"
 		return 1
 	fi
 	umount_all
 	if ! OP=$(chcon u:object_r:apk_data_file:s0 "$RVPATH" 2>&1); then
-		err "Error chcon: '$OP'"
+		ch_desc_err "Error chcon: '$OP'"
 		return 1
 	fi
 	mount -o bind "$RVPATH" "${1}/base.apk"
@@ -67,7 +67,7 @@ mount_rv() {
 
 mount_rv_now() {
 	if ! BASEPATH=$(get_basepath); then
-		err "app not installed: '$BASEPATH'"
+		ch_desc_err "App not installed: '$BASEPATH'"
 		return 1
 	fi
 	mount_rv "$BASEPATH"
